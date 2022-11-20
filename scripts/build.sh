8c124119cf9a876e9da77e6c94ccebbffa5cef29
phuongtd <trandinhphuongwp>
2022-11-21 00:01:52 +0700

    OKE

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ set -e
 
 BUILD_NUMBER=$1
 
-docker.exe build -t 798426491631.dkr.ecr.ap-southeast-1.amazonaws.com/ecr_phuongtd:$BUILD_NUMBER .
-docker.exe push 798426491631.dkr.ecr.ap-southeast-1.amazonaws.com/ecr_phuongtd:$BUILD_NUMBER
+/usr/bin/com.docker.cli build -t 798426491631.dkr.ecr.ap-southeast-1.amazonaws.com/ecr_phuongtd:$BUILD_NUMBER .
+/usr/bin/com.docker.cli push 798426491631.dkr.ecr.ap-southeast-1.amazonaws.com/ecr_phuongtd:$BUILD_NUMBER
 
--- scripts/login-ecr.sh
+++ scripts/login-ecr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-/usr/local/bin/aws ecr get-login-password --region ap-southeast-1 | docker.exe login --username AWS --password-stdin 798426491631.dkr.ecr.ap-southeast-1.amazonaws.com
+/usr/local/bin/aws ecr get-login-password --region ap-southeast-1 | /usr/bin/com.docker.cli login --username AWS --password-stdin 798426491631.dkr.ecr.ap-southeast-1.amazonaws.com
