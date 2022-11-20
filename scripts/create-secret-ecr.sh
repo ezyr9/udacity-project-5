af10aa0fb71605b8ca85cbc9f78cd1c75a600fc8
phuongtd <trandinhphuongwp>
2022-11-20 23:06:20 +0700

    OK

--- scripts/create-secret-ecr.sh
+++ scripts/create-secret-ecr.sh
@@ -4,7 +4,4 @@ set -e
 /usr/local/bin/kubectl create secret docker-registry phuongtd-secret-ecr \
     --docker-server=https://798426491631.dkr.ecr.ap-southeast-1.amazonaws.com \
     --docker-username=AWS \
-    --docker-password=$(/usr/local/bin/aws ecr get-login-password --region ap-southeast-1) \
-
-
-/usr/local/bin/kubectl get all 
+    --docker-password=$(/usr/local/bin/aws ecr get-login-password --region ap-southeast-1)
--- scripts/update-eks-config.sh
+++ scripts/update-eks-config.sh
@@ -4,3 +4,5 @@ set -e
 /usr/local/bin/aws eks update-kubeconfig \
     --region ap-southeast-1 \
     --name udacity-cluster
+
+sudo sed -e "/command: aws/s?aws?$(which aws)?" -i /var/lib/jenkins/.kube/config
